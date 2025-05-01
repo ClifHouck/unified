3c7cf7f5ef8409c9f2ea08bea87d88eaf2393375
Clif Houck <me@clifhouck.com>
2025-05-01 16:56:09 -0500

    Move protect types into protect.go
    Signed-off-by: Clif Houck <me@clifhouck.com>

--- types/protect.go
+++ types/protect.go
@@ -6,3 +6,47 @@ type ProtectV1 interface {
 
 	// FIXME: Fill-in rest.
 }
+
+type ProtectInfo struct {
+	ApplicationVersion string `json:"applicationVersion"`
+}
+
+type Camera struct {
+	ID           string `json:"id"`
+	ModelKey     string `json:"modelKey"`
+	State        string `json:"state"`
+	Name         string `json:"name"`
+	IsMicEnabled bool   `json:"isMicEnabled"`
+	OsdSettings  struct {
+		IsNameEnabled  bool `json:"isNameEnabled"`
+		IsDateEnabled  bool `json:"isDateEnabled"`
+		IsLogoEnabled  bool `json:"isLogoEnabled"`
+		IsDebugEnabled bool `json:"isDebugEnabled"`
+	} `json:"osdSettings"`
+	LedSettings struct {
+		IsEnabled bool `json:"isEnabled"`
+	} `json:"ledSettings"`
+	LcdMessage struct {
+		Type    string `json:"type"`
+		ResetAt int    `json:"resetAt"`
+		Text    string `json:"text"`
+	} `json:"lcdMessage"`
+	MicVolume        int    `json:"micVolume"`
+	ActivePatrolSlot int    `json:"activePatrolSlot"`
+	VideoMode        string `json:"videoMode"`
+	HdrType          string `json:"hdrType"`
+	FeatureFlags     struct {
+		SupportFullHdSnapshot bool     `json:"supportFullHdSnapshot"`
+		HasHdr                bool     `json:"hasHdr"`
+		SmartDetectTypes      []string `json:"smartDetectTypes"`
+		SmartDetectAudioTypes []string `json:"smartDetectAudioTypes"`
+		VideoModes            []string `json:"videoModes"`
+		HasMic                bool     `json:"hasMic"`
+		HasLedStatus          bool     `json:"hasLedStatus"`
+		HasSpeaker            bool     `json:"hasSpeaker"`
+	} `json:"featureFlags"`
+	SmartDetectSettings struct {
+		ObjectTypes []string `json:"objectTypes"`
+		AudioTypes  []string `json:"audioTypes"`
+	} `json:"smartDetectSettings"`
+}
